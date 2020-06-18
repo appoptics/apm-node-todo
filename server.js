5d8e8d11eb837d7fe59d1076292d2e00b1b8e32a
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2020-06-18 15:02:06 -0700

    server.js - mostly metrics
    - don't open https by default
    - wip: guess at membase
    - add service tag to startup annotation
    - add metrics, some conditional

--- server.js
+++ server.js
@@ -49,7 +49,8 @@ const fs = require('fs')
 // request wraps the individual request constructors and fills in ao for them
 // there's certainly a better way to do this but i haven't figured it out yet.
 const Requests = require('./lib/requests')(ao)
-const accounting = new Requests.Accounting()
+const kAcctSecs = 10;
+const accounting = new Requests.Accounting({interval: kAcctSecs});
 
 // get host name for metrics and general status/config
 let hostname = fs.readFileSync('/etc/hostname', 'utf8')
@@ -105,12 +106,24 @@ if ('trace-mode' in argv && argv.t in modeMap) {
 //  ao.cfg.insertTraceIdsIntoLogs = argv.insert;
 //  ao.cfg.insertTraceIdsIntoMorgan = argv.insert;
 //}
+const sk = process.env.APPOPTICS_SERVICE_KEY || 'xyzzy:xyzzy';
+const [serviceKey, service] = sk.split(':');
 
-//
+//========================================================================
 // finally host metrics configuration
-//
+//========================================================================
 
-const minutesToMs = m => m * 60000
+// these are considered the "base" and are filled in at the end of initialization.
+// that way most of the application's base memory usage has been allocated. oboe
+// doesn't expose the event queue size (currently 10,000) so it's saved just after
+// initialization as an approximation of the value.
+const memBase = {
+  rss: 0,
+  heapUsed: 0,
+  heapTotal: 0,
+  eventQueueFree: 0,
+};
+
 //==================================================================================
 // if supplied, metrics must be a valid appoptics token (not service key) or metrics
 // won't be collected.
@@ -124,28 +137,126 @@ if (argv.metrics) {
   const m = new Metrics(
     argv.metrics,
     `https://${prefix}api.appoptics.com/v1/measurements`,
-    {image_name: `${hostname}-${ao.version}`}
+    {
+      image_name: `${hostname}-${ao.version}`,
+      service,
+    }
   )
 
-  function makeMetrics (prefix, object) {
+  function addMetrics (metrics, prefix, object) {
+    // first make the metrics based on object, using prefix
     const o = {};
     const properties = Object.keys(object);
     for (let i = 0; i < properties.length; i++) {
       o[prefix + properties[i]] = object[properties[i]];
     }
-    return o;
+    return Object.assign(metrics, o);
   }
 
-  function getMetrics () {
-    // the next two are undefined if no transactions have taken place
+  //
+  //function addUrlMetrics (prefix, urlMetricsMap, metrics) {
+  //  for (let url of urlMetricsMap.keys()) {
+  //    const m = urlMetricsMap.get(url);
+  //    url = url.replace(/\//g, ':');
+  //    url = url.replace(/\*/g, '-');
+  //    metrics[prefix + url + '.count'] = m.count;
+  //    metrics[prefix + url + '.time'] = m.time / 1e3 / m.count;
+  //    metrics[prefix + url + '.events'] = m.events / m.count;
+  //  }
+  //}
+
+  let prevTopSpansExited = 0;
+
+  function getMetrics (deltaTime) {
+    const stats = accounting.get(kAcctSecs);
     const metrics = {
-      'todo.cpu.perTransaction': accounting.get().cpuUserPerTx[minutesToMs(1)] || 0,
-      'todo.apm.lastRate': accounting.get().lastRate || 0,
+      [`todo.${kAcctSecs}sec.total.transactions`]: stats.totalAverages,
+      [`todo.${kAcctSecs}sec.total.sampled`]: stats.sampledAverages,
+      [`todo.${kAcctSecs}sec.cpuUser.perTransaction`]: stats.cpuUserPerTx,
+      [`todo.${kAcctSecs}sec.cpuSystem.perTransaction`]: stats.cpuSystemPerTx,
+      [`todo.${kAcctSecs}sec.apm.lastRate`]: stats.lastRate || 0,
+      //[`todo.${kAcctSecs}sec.fillRequests`]: stats.fillRequests,
+      //[`todo.${kAcctSecs}sec.copyFills`]: stats.copyFills,
+      //[`todo.${kAcctSecs}sec.syncFills`]: stats.syncFills,
+      //[`todo.${kAcctSecs}sec.bufferFills`]: stats.bufferFills,
     };
-    Object.assign(metrics, makeMetrics('todo.memory.', process.memoryUsage()));
-    Object.assign(metrics, makeMetrics('todo.memory.v8.heap.', v8.getHeapStatistics()));
+    const mu = process.memoryUsage();
+    addMetrics(metrics, 'todo.memory.', mu);
+    addMetrics(metrics, 'todo.memory.v8.heap.', v8.getHeapStatistics());
+
+    // reset these to the lowest values seen. that's the effective
+    // base.
+    if (memBase.rss > mu.rss) memBase.rss = mu.rss;
+    if (memBase.heapTotal > mu.heapTotal) memBase.heapTotal = mu.heapTotal;
+    if (memBase.heapUsed > mu.heapUsed) memBase.heapUsed = mu.heapUsed;
+
+    const delta = {
+      rss: mu.rss - memBase.rss,
+      heapUsed: mu.heapUsed - memBase.heapUsed,
+      heapTotal: mu.heapTotal - memBase.heapTotal,
+    }
+
+    // take a stab at delta memory usage.
+    addMetrics(metrics, 'todo.memory.delta.', delta);
+
+    // eventsCreated: 0,       // total events created
+    // eventsActive: 0,        // event.send() has not been called
+
+    // spansCreated: 0,        // total spans created
+    // topSpansCreated: 0,     // total entry spans (traces) created
+    // topSpansActive: 0,      // topSpans: span.enter() called but not span.exit()
+    // otherSpansActive: 0,    // not-topSpan: span.enter() called but not span.exit()
+
+    //
+    // N.B. many metrics are protected by checks to see if they exist so todo
+    // remains compatible with previous versions of the agent that don't have
+    // those metrics.
+    //
+
+    if (ao._stats && ao._stats.event) {
+      addMetrics(metrics, 'todo.agent.event.', ao._stats.event);
+      addMetrics(metrics, 'todo.agent.span.', ao._stats.span);
+      const topSpansExited = ao._stats.span.topSpansExited - prevTopSpansExited;
+      prevTopSpansExited = ao._stats.span.topSpansExited;
+      metrics['todo.agent.span.tracesPerSecond'] = topSpansExited / (deltaTime / 1000);
+
+      // change Infinity to log counts at some number of
+      // active events.
+      if (ao._stats.event.active >= 20000 && ao._stats.unsentEvents) {
+        const counts = {};
+        for (const u of ao._stats.unsentEvents) {
+          const k = `${u[0].Layer}:${u[0].Label}`;
+          if (!(k in counts)) {
+            counts[k] = 1;
+          } else {
+            counts[k] += 1;
+          }
+        }
+        console.log(counts);
+        process.exit(1);
+      }
+    }
+
+    if (ao.addon) {
+      if (ao.addon.Event.getEventStats) {
+        const es = ao.addon.Event.getEventStats(0x1);
+        addMetrics(metrics, 'todo.aob.eventCounts.', es);
+      }
+      const oboeStats = ao.addon.Config.getStats();
+      metrics['todo.oboe.eventQueue'] = 10000 - oboeStats.eventQueueFree;
+    }
     // skip this array-containing object for now.
-    //Object.assign(metrics, makeMetrics('todo.memory.v8.heap.space.', v8.getHeapSpaceStatistics()));
+    //addMetrics(metrics, 'todo.memory.v8.heap.space.', v8.getHeapSpaceStatistics());
+    //const spanMetrics = ao.Span.getMetrics('clear');
+    //addMetrics(metrics, 'todo.span.', spanMetrics);
+    //metrics['todo.span.msPerTopSpan'] = spanMetrics.topSpanTime / 1e3 / spanMetrics.topSpanExits;
+    //metrics['todo.span.msPerHttpSpan'] = spanMetrics.httpSpanTime / 1e3 / spanMetrics.httpSpanCount;
+    //metrics['todo.span.eventsPerSpan'] = spanMetrics.topSpanEvents / spanMetrics.topSpanExits;
+    //
+    //if (ao.Span.urlMap) {
+    //  addUrlMetrics('todo.span.endpoints.', ao.Span.urlMap, metrics);
+    //  ao.Span.urlMap = new Map();
+    //}
     return {metrics};
   }
 
@@ -155,7 +266,7 @@ if (argv.metrics) {
     }
   }
 
-  const ctx = m.sendOnInterval(5000, getMetrics, metricsSent);
+  const ctx = m.sendOnInterval(10000, getMetrics, metricsSent);
 
   // could work on restarting but not sure why
   ctx.promise.catch(e => {
@@ -165,7 +276,6 @@ if (argv.metrics) {
 
 // setup annotations as well
 const Annotations = require('./lib/annotations');
-const serviceKey = process.env.APPOPTICS_SERVICE_KEY.split(':')[0];
 const annotationsOpts = {};
 if (staging) {
   annotationsOpts.url = 'https://my-stg.appoptics.com/v1/annotations';
@@ -191,36 +301,13 @@ if (argv.gc) {
   }
 }
 
-//=====================================================================================
-// if headdumps are requested set them up too. the argument is in minutes so convert to
-// milliseconds for setInterval().
-//=====================================================================================
-let hdInterval; // eslint-disable-line no-unused-vars
-if (argv['heap-dump']) {
-  const mkdirp = require('mkdirp');
-  const heapdump = require('heapdump');
-  mkdirp('./heapdump', function (err) {
-    if (err) {
-      console.error('cannot mkdirp ./heapdump');
-      return;
-    }
-    hdInterval = setInterval(function () {
-      // make a more intelligible filename
-      const ts = new Date().toISOString();
-      const filename = `./heapdump/heapdump-${ts}.heapsnapshot`;
-      heapdump.writeSnapshot(filename, function (err, filename) {
-        // don't do anything right now.
-      })
-    }, argv['heap-dump'] * 60 * 1000);
-  })
-}
 
 //
 // finally get the ports needed
 //
 let port
 let host
-let httpsPort = 8443;
+let httpsPort = argv['https-port'];
 if (!argv.heroku) {
   host = webServerHost.split(':')
   port = +host[1]
@@ -240,8 +327,9 @@ const staticFiles = {
   '/bower_components': '/bower_components'
 }
 
-// get the lower level api that knows nothing of web server frameworks
-const todoapi = new Requests.TodoApi(mongoHost)
+// get the lower level api that knows nothing of web server frameworks.
+const dbName = process.env.TODO_DBNAME || service.replace(/\./g, '-');
+const todoapi = new Requests.TodoApi(mongoHost, {dbName});
 
 // get the Event.last formatter for insertion into logs
 const traceToken = ao.getFormattedTraceId;
@@ -253,7 +341,7 @@ const options = {
   todoapi,
   host,
   httpPort: port,
-  httpsPort,
+  httpsPort: !argv['no-https'] && httpsPort,
   traceToken,
   logger,
   awsKinesisOpts: {newOptions: {correctClockSkew: true}}, // maxRetries: n (default is maybe 3?)
@@ -262,6 +350,13 @@ const options = {
 const frameworkSelection = argv.f || 'express'
 let framework
 let config
+
+
+//==============================================================================
+//==============================================================================
+// get the server running ======================================================
+//==============================================================================
+//==============================================================================
 
 if (frameworkSelection === 'express') {
   framework = require('./frameworks/express')
@@ -292,7 +387,9 @@ config.then(r => {
   // https is optional
   if (r.httpsStatus) {
     console.warn('https failed to initialize', r.httpsStatus)
-    httpsPort = 'NA'
+    httpsPort = 'NA';
+  } else if (argv['no-https']) {
+    httpsPort = 'NA';
   }
 
   // http is not optional
@@ -332,16 +429,16 @@ config.then(r => {
   }
   annotations.send(streamName, title, opts);
 
+  // set base levels now so there's data of some sort right away.
+  const m = process.memoryUsage();
+  memBase.rss = m.rss;
+  memBase.heapTotal = m.heapTotal;
+  memBase.heapUsed = m.heapUsed;
+
 }).catch(e => {
   console.error(`${frameworkSelection} framework initialization error`, e)
   process.exit(1)
 })
-
-//==============================================================================
-//==============================================================================
-// get the server running ======================================================
-//==============================================================================
-//==============================================================================
 
 // taken from appoptics test suite. these are not valid for any real
 // servers - only used for local testing.
@@ -350,25 +447,3 @@ const sslInfo = { // eslint-disable-line
   cert: '-----BEGIN CERTIFICATE-----\nMIICWDCCAcGgAwIBAgIJAPIHj8StWrbJMA0GCSqGSIb3DQEBCwUAMEUxCzAJBgNV\nBAYTAkFVMRMwEQYDVQQIDApTb21lLVN0YXRlMSEwHwYDVQQKDBhJbnRlcm5ldCBX\naWRnaXRzIFB0eSBMdGQwHhcNMTQwODI3MjM1MzUwWhcNMTQwOTI2MjM1MzUwWjBF\nMQswCQYDVQQGEwJBVTETMBEGA1UECAwKU29tZS1TdGF0ZTEhMB8GA1UECgwYSW50\nZXJuZXQgV2lkZ2l0cyBQdHkgTHRkMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKB\ngQCsJU2dO/K3oQEh9wo60VC2ajCZjIudc8cqHl9kKNKwc9lP4Rw9KWso/+vHhkp6\nCmx6Cshm6Hs00rPgZo9HmY//gcj0zHmNbagpmdvAmOudK8l5NpzdQwNROKN8EPoK\njlFEBMnZj136gF5YAgEN9ydcLtS2TeLmUG1Y3RR6ADjgaQIDAQABo1AwTjAdBgNV\nHQ4EFgQUTqL/t/yOtpAxKuC9zVm3PnFdRqAwHwYDVR0jBBgwFoAUTqL/t/yOtpAx\nKuC9zVm3PnFdRqAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOBgQBn1XAm\nAsVdXKr3aiZIgOmw5q+F1lKNl/CHtAPCqwjgntPGhW08WG1ojhCQcNaCp1yfPzpm\niaUwFrgiz+JD+KvxvaBn4pb95A6A3yObADAaAE/ZfbEA397z0RxwTSVU+RFKxzvW\nyICDpugdtxRjkb7I715EjO9R7LkSe5WGzYDp/g==\n-----END CERTIFICATE-----'
 }
 
-
-//const methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
-
-/*
-
-if (ao.setCustomTxNameFunction && (argv.c || argv.custom)) {
-  ao.setCustomTxNameFunction('express', customExpressTxName)
-}
-
-function customExpressTxName (req, res) {
-  // ignore global routes
-  if (req.route.path === '*') {
-    console.log('called for * route')
-    return ''
-  }
-  console.log('req.method', req.method, 'r.r.p', req.route.path, 'url', req.url, 'r.r.m', req.route.methods, 'ourl', req.originalUrl)
-  const customTxname = 'TODO-' + req.method + req.route.path
-  console.log('custom name: ', customTxname)
-  return customTxname
-}
-
-// */
